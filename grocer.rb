4e220a59b222fc4f0edf91a33a95ed1b35318509
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-30 23:04:02 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,71 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  cart.reduce({}) do |memo, element|
+    key = element.keys[0]
+    if memo.has_key?(key)
+       element[key][:count] += 1
+    else
+       element[key][:count] = 1
+       memo[key]=element.values[0]
+    end
+    memo
+  end
 end
 
+
+=begin
 def apply_coupons(cart, coupons)
-  # code here
+    coupons.each do |hash|
+      if cart.values[0][:clearance] == TRUE
+        item = hash[:item]
+        cost = hash[:cost]
+        num = hash.values[1]
+        count = cart[item][:count]
+        price = cart.values[0][:price]
+     cart["#{item} W/COUPON"] = {}
+     cart["#{item} W/COUPON"][:price] = cost / count
+     cart["#{item} W/COUPON"][:clearance] = TRUE
+     cart[hash[:item]][:count] -= hash[:num]
+     end
+    end
+    return cart
+end
+=end 
+
+
+
+def apply_coupons(cart, coupons)
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
+        else
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.keys.each do |item|
+    if cart[item][:clearance]
+      cart[item][:price] = (cart[item][:price]*0.80).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+ 
+  
 end
